4b1b8f79a1d20ffe3c81dac1e462a2242e703f43
tead1234 <35370337+tead1234@users.noreply.github.com>
2026-03-20 19:39:26 +0900

    Title: 국영수 순이지, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260320/국영수 순이지/korean-english-math-order.py
+++ 260320/국영수 순이지/korean-english-math-order.py
@@ -2,16 +2,18 @@ n = int(input())
 name = []
 height = []
 weight = []
+o = []
 
 for _ in range(n):
-    n_i, h_i, w_i = input().split()
+    n_i, h_i, w_i, o_i = input().split()
     name.append(n_i)
     height.append(int(h_i))
     weight.append(int(w_i))
+    o.append(int(o_i))
 
 # Please write your code here.
-L = list(zip(name, height, weight))
-L.sort(key = lambda x: (-x[0], -x[1], -x[2]))
+L = list(zip(name, height, weight, o))
+L.sort(key = lambda x: (-x[1], -x[2], -x[3]))
 
 for  i in L:
     print(i[0], end = " ")
