492cc07345aabbdb9143121ff9a37210eb0d6a59
tead1234 <35370337+tead1234@users.noreply.github.com>
2026-03-24 15:33:11 +0900

    Title: 좌우 반전시키기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260324/좌우 반전시키기/flip-left-and-right.py
+++ 260324/좌우 반전시키기/flip-left-and-right.py
@@ -13,7 +13,11 @@ def change(i):
              a[i+1] = 1
         else:
             a[i+1] =0
-    a[i] = 1
+    if a[i] == 1 :
+        a[i] = 0
+    else:
+        a[i] = 1
+    # a[i] = 1
     # print(i, a)
 
 
@@ -22,7 +26,7 @@ ans = 0
 for i in range(1, N):
     # print(a[i])
 
-    if (i-1) >=0 and a[i-1] == 0:
+    if  a[i-1] == 0:
         # print("진입", i)
         change(i)
         ans += 1
