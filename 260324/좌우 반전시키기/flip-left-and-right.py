04a99534fb67084d7d9004210513a94c0788b9db
tead1234 <35370337+tead1234@users.noreply.github.com>
2026-03-24 12:53:42 +0900

    Title: 좌우 반전시키기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260324/좌우 반전시키기/flip-left-and-right.py
@@ -0,0 +1,39 @@
+N = int(input())
+a = list(map(int,input().split()))
+# b = list(input())
+### 중간에 리스트가 바뀌는 경우엔 어떻게 대처?
+# Please write your code here.
+def change(i):
+    if (i - 1) >= 0:
+        if a[i-1] ==0:
+             a[i-1] = 1
+        else:
+            a[i-1] =0
+    if (i + 1) < N:
+        if a[i+1] ==0:
+             a[i+1] = 1
+        else:
+            a[i+1] =0
+    a[i] = 1
+    # print(i, a)
+
+
+ind=  0
+ans = 0
+for i in range(N):
+    # print(a[i])
+
+    if a[i] == 0:
+        # print("진입", i)
+        change(i)
+        ans += 1
+# for i in a:
+#     if i == 0:
+#         ind = 1
+
+if a[N-1] == 0:
+    print(-1)
+else:
+    print(ans)
+
+
