b5f27574e7116c2a3d66d42b18199a018ffe6480
tead1234 <35370337+tead1234@users.noreply.github.com>
2026-03-30 00:39:14 +0900

    Title: 겹치지 않는 선분 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260330/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 260330/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -6,17 +6,30 @@ lines = [tuple(map(int, input().split())) for _ in range(n)]
 
 lines.sort(key= lambda x:(x[0], -x[1]))
 ans = 0
-for i in range(n-1):
-    target_line = lines[i]
-    compare_line = lines[i+1]
-    t_x, t_y = target_line
-    c_x, c_y = compare_line
+겹치는리스트 = [0] * n
+for i in range(n):
+    flag = False
+    for j in range(i+1, n):
+        if i == n and j == n:
+            continue
+        target_line = lines[i]
+        compare_line = lines[j]
+        t_x, t_y = target_line
+        c_x, c_y = compare_line
     ### 겹치는지 아닌지 판단하는 로직이 더필요
     ### 무조건 겹치는경우
-    if t_y > c_y:
-        continue
-    else:
-        ans+=1
-        # continue
+        if 겹치는리스트[i] ==1 or 겹치는리스트[j]==1:
+            flag = True
+            break
+        if t_y > c_y: 
+            flag = True
+            겹치는리스트[i] = 1
+            겹치는리스트[j] = 1
+            ### 타겟을 제외한 나머지 루프에서 제외
+            break
+    if not flag:
+        ## 겹치는거 없음
+        ans += 1
+            # continue
         
 print(ans)
