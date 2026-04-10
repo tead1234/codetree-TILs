8d87e2c1cb362587829d59e56e8fdb23094cc246
tead1234 <35370337+tead1234@users.noreply.github.com>
2026-04-10 19:24:19 +0900

    Title: 최대 수 만들기, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260410/최대 수 만들기/make-biggest-num.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+### 먼저 앞자리 수를 비교 
+from functools import cmp_to_key
+
+def compare(x,y):
+    
+    x_num = int(str(x) + str(y))
+    y_num = int(str(y) + str(x))
+    if x_num < y_num:
+        return -1
+    elif y_num < x_num:
+        return 1
+    return 0
+
+arr.sort(key = cmp_to_key(compare), reverse = True)
+answer = ""
+# print(arr)
+for a in arr:
+    answer += str(a)
+print(answer)
+
