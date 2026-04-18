4b123597c38d3c9a237ad1fa75a1f3462f528252
tead1234 <35370337+tead1234@users.noreply.github.com>
2026-04-18 22:16:27 +0900

    Title: 적석과 흑석 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260418/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 260418/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,39 +1,41 @@
-C, N = map(int, input().split())
-T = [int(input()) for _ in range(C)]
-AB = [tuple(map(int, input().split())) for _ in range(N)]
-A = [ab[0] for ab in AB]
-B = [ab[1] for ab in AB]
-
-# Please write your code here.
-### 아까와 같은 문제
-
-
-
-### 빨간돌이 어떤 검은 돌들을 만족시킬 수 있는지 정리
-from collections import deque
 import heapq
+import sys
 
+# 입력 속도 향상
+input = sys.stdin.readline
 
-# AB.sort(key = lambda x : (x[0],x[1]))
-heapq.heapify(AB)
-T.sort()
+def solve():
+    C, N = map(int, input().split())
+    
+    # 빨간 돌 입력 및 정렬
+    T = [int(input()) for _ in range(C)]
+    T.sort()
+    
+    # 검은 돌 입력 및 A 기준 정렬
+    AB = []
+    for _ in range(N):
+        AB.append(list(map(int, input().split())))
+    AB.sort()  # A[0] 기준 오름차순 정렬
 
-
-ans = 0
-AB = deque(AB)
-for t in T:
-    # print(t)
-    temp = []
-    while AB:
-        a,b = heapq.heappop(AB)
-        # print(a,b)
-        if a <= t <= b:
-            # print(t)
+    ans = 0
+    active_candidates = [] # B_j 값을 담을 최소 힙
+    stone_idx = 0
+    
+    for t in T:
+        # 1. 현재 빨간 돌 t보다 시작점(A)이 작거나 같은 검은 돌을 모두 후보 힙에 추가
+        while stone_idx < N and AB[stone_idx][0] <= t:
+            heapq.heappush(active_candidates, AB[stone_idx][1])
+            stone_idx += 1
+        
+        # 2. 후보들 중 끝점(B)이 현재 t보다 작은 돌은 매칭 불가능하므로 제거
+        while active_candidates and active_candidates[0] < t:
+            heapq.heappop(active_candidates)
+            
+        # 3. 유효한 후보가 있다면 가장 빨리 끝나는(B가 작은) 돌과 매칭
+        if active_candidates:
+            heapq.heappop(active_candidates)
             ans += 1
-            break
-        if t < a:
-            temp.append((a,b))
-            # heapq.heappush(AB, (a,b))
-    for tem in temp:
-        heapq.heappush(AB,tem)
-print(ans)
+            
+    print(ans)
+
+solve()
